770bdafcf06148479c5de1143a86d81bc4048fc4
tore-statsig <74584483+tore-statsig@users.noreply.github.com>
2021-06-02 10:14:06 -0700

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,8 +16,8 @@ repositories {
 dependencies {
     testImplementation(kotlin("test-junit"))
     implementation("com.google.code.gson:gson:2.8.6")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.2.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.4.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.5.0")
     implementation("com.squareup.okhttp3:okhttp:4.9.1")
     implementation("com.blueconic:browscap-java:1.3.6")
     implementation("org.apache.maven:maven-artifact:3.8.1")
