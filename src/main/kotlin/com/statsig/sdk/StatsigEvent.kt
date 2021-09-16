ce9d0a277655578fa1f8d41f15e924af1b7f3c1e
tore-statsig <74584483+tore-statsig@users.noreply.github.com>
2021-09-16 15:37:43 -0700

    Test SDK e2e with network mocked out

--- src/main/kotlin/com/statsig/sdk/StatsigEvent.kt
+++ src/main/kotlin/com/statsig/sdk/StatsigEvent.kt
@@ -3,7 +3,7 @@ package com.statsig.sdk
 import com.google.gson.annotations.SerializedName
 
 data class StatsigEvent(
-    @SerializedName("eventName") private val eventName: String,
+    @SerializedName("eventName") val eventName: String,
     @SerializedName("value") val eventValue: Any? = null,
     @SerializedName("metadata") val eventMetadata: Map<String, String>? = null,
     @SerializedName("user") var user: StatsigUser? = null,
