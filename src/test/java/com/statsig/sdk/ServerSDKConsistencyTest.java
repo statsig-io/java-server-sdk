b7ca7a346a2604b4d989517e5d4916b116e4b915
tore-statsig <74584483+tore-statsig@users.noreply.github.com>
2021-11-18 15:59:50 -0800

    revert

--- src/test/java/com/statsig/sdk/ServerSDKConsistencyTest.java
+++ src/test/java/com/statsig/sdk/ServerSDKConsistencyTest.java
@@ -12,7 +12,6 @@ import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.Future;
 
