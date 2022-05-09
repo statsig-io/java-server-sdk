acb5bd56781ec67467deff4291d36ddd775bb35a
Jiakan Wang <jkw@statsig.io>
2022-05-09 16:25:51 -0700

    remove version bump

--- src/main/kotlin/com/statsig/sdk/StatsigServer.kt
+++ src/main/kotlin/com/statsig/sdk/StatsigServer.kt
@@ -130,7 +130,7 @@ sealed class StatsigServer {
     }
 }
 
-private const val VERSION = "0.14.2"
+private const val VERSION = "0.14.0"
 
 private class StatsigServerImpl(serverSecret: String, private val options: StatsigOptions) :
         StatsigServer() {
