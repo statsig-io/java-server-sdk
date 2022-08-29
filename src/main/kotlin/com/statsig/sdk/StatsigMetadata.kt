d166699575fe436d7646e8bb955598f231ab1774
tore-statsig <74584483+tore-statsig@users.noreply.github.com>
2022-08-29 15:30:09 -0700

    bump v0.18.1

--- src/main/kotlin/com/statsig/sdk/StatsigMetadata.kt
+++ src/main/kotlin/com/statsig/sdk/StatsigMetadata.kt
@@ -2,7 +2,7 @@ package com.statsig.sdk
 
 import java.util.Properties
 
-private const val VERSION = "0.18.0"
+private const val VERSION = "0.18.1"
 
 internal class StatsigMetadata {
     companion object {
