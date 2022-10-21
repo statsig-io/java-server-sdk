bb5488084192248a7520d205bb0bb3665d647fed
tore-statsig <74584483+tore-statsig@users.noreply.github.com>
2022-10-21 10:12:30 -0700

    bump ip3country

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.6.4")
     implementation("com.squareup.okhttp3:okhttp:4.10.0")
     implementation("com.github.ua-parser:uap-java:1.5.3")
-    implementation("com.statsig:ip3country:0.1.1")
+    implementation("com.statsig:ip3country:0.1.2")
 }
 
 tasks.test {
