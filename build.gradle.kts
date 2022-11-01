d09f4e2b14050cb17ca33af1a9ee9a5e69ef2475
tore-statsig <74584483+tore-statsig@users.noreply.github.com>
2022-11-01 08:49:34 -0700

    temporarily remove maven pub

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,6 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.6.0"
     idea
-    id("com.vanniktech.maven.publish") version "0.22.0"
     id("org.jlleitschuh.gradle.ktlint") version "11.0.0"
     id("maven-publish")
 }
