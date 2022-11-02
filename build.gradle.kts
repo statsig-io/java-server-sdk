00516d5542f0cf0ef6c5c36dd08ce04fe15c5611
tore-statsig <74584483+tore-statsig@users.noreply.github.com>
2022-11-02 10:14:50 -0700

    Support java8

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
     kotlin("jvm") version "1.6.0"
     idea
     id("org.jlleitschuh.gradle.ktlint") version "11.0.0"
+    id("com.vanniktech.maven.publish") version "0.22.0"
     id("maven-publish")
 }
 
@@ -68,10 +69,10 @@ inline fun <reified C> Project.configure(name: String, configuration: C.() -> Un
 }
 
 tasks.withType<KotlinCompile>() {
-    kotlinOptions.jvmTarget = "11"
+    kotlinOptions.jvmTarget = "1.8"
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
