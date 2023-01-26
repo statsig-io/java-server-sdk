825ebd8ce523855b80fc0beefabf940407ab4ea5
daniel-statsig <daniel@statsig.com>
2023-01-25 17:59:57 -0800

    bump version

--- src/main/kotlin/com/statsig/sdk/StatsigMetadata.kt
+++ src/main/kotlin/com/statsig/sdk/StatsigMetadata.kt
@@ -2,7 +2,7 @@ package com.statsig.sdk
 
 import java.util.Properties
 
-private const val VERSION = "1.3.1"
+private const val VERSION = "1.3.2"
 
 internal class StatsigMetadata {
     companion object {
