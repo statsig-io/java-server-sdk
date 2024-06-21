f016f9add7bf5c0e09bd485f04f15b6abf036208
Weihao Ding <158090588+weihao-statsig@users.noreply.github.com>
2024-06-21 16:28:09 -0700

    revert: delete inline ip3 country look up files (#305)

--- src/main/kotlin/com/statsig/sdk/CountryLookup.kt
+++
@@ -1,5 +0,0 @@
-package com.statsig.sdk
-
-private const val IP_TABLE_FILE: String = "ip_supalite.table"
-private const val NULL_CC: String = "--"
-private const val LOOKUP_TABLE_TERMINATOR: Int = '*'.code
