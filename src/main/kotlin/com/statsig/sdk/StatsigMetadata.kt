1ca58fd76a668d5ccf4819222b9bee2e06d480bf
kenny-statsig <111380336+kenny-statsig@users.noreply.github.com>
2024-07-24 23:40:10 -0700

    [release] 1.23.0 - Target app condition (#324)
    ### New Features
      - Support a new target app condition for evaluations
      ### Fixes
    - Fixed a bug where some events in flight were being dropped on
    shutdown.
    
    
    
    
    
    >Included In This Release
    >- a2d40523fb890cc9834fd6a4ea48709dac7512b9 kenny-statsig
    >   - target app condition (#321)
    >- f03d71fc60674192b6ba9d08dad8b8d86ed180b5 tore-statsig
    >   - fix: dont overwrite values if lcut is older (#315)
    >- 3866984a7eece9c4d228acb70e9fdbf127ffe281 Weihao Ding
    >   - fix: do not cancel ongoing request when we shutdown (#319)
    >- bb27ffc2ef55fd070d345f755e12e7cc79d6eada Weihao Ding
    >   - Revert "fix: event-flushing when shutdown" (#318)
    >- b565b181aa405e68e2f550e6b8f525ab95864f11 Weihao Ding
    >   - fix: event-flushing when shutdown (#316)

--- src/main/kotlin/com/statsig/sdk/StatsigMetadata.kt
+++ src/main/kotlin/com/statsig/sdk/StatsigMetadata.kt
@@ -6,7 +6,7 @@ import com.google.gson.annotations.SerializedName
 import java.util.Properties
 import java.util.UUID
 
-private const val VERSION = "1.22.0"
+private const val VERSION = "1.23.0"
 
 internal data class StatsigMetadata(@SerializedName("sdkType") var sdkType: String = "java-server", @SerializedName("sessionID") var sessionID: String = UUID.randomUUID().toString(), @SerializedName("languageVersion") var languageVersion: String = System.getProperty("java.version"), @SerializedName("exposureLoggingDisabled") var exposureLoggingDisabled: Boolean? = null) {
     @SerializedName("sdkVersion")
