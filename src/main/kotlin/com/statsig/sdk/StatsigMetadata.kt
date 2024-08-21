7443c357c78616142de9257af9e4c55c877ca700
Xin Li <137219293+xinlili-statsig@users.noreply.github.com>
2024-08-20 21:42:21 -0700

    [release] 1.25.0 - refac: Refactor grpc with generated java (#350)
    ### Improvements
    - Refactor grpc using generated java file instead of kotlin since java
    is better maintained.
    
    
    
    
    
    
    >Included In This Release
    >- bb49206b2bb7f841c1184659b78a4b44828489b7 Xin Li
    > - refactor: Use java generated file instead of kotlin to remove
    dependency (#349)

--- src/main/kotlin/com/statsig/sdk/StatsigMetadata.kt
+++ src/main/kotlin/com/statsig/sdk/StatsigMetadata.kt
@@ -4,7 +4,7 @@ import com.google.gson.annotations.SerializedName
 import java.util.Properties
 import java.util.UUID
 
-private const val VERSION = "1.24.1"
+private const val VERSION = "1.25.0"
 
 internal data class StatsigMetadata(@SerializedName("sdkType") var sdkType: String = "java-server", @SerializedName("sessionID") var sessionID: String = UUID.randomUUID().toString(), @SerializedName("languageVersion") var languageVersion: String = System.getProperty("java.version"), @SerializedName("exposureLoggingDisabled") var exposureLoggingDisabled: Boolean? = null) {
     @SerializedName("sdkVersion")
