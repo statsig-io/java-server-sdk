d489be1aac0f0ad74076e72b04e16fdf156666f6
Weihao Ding <158090588+weihao-statsig@users.noreply.github.com>
2024-12-13 16:18:56 -0800

    [release] 1.34.0 - Add Sampling to Exposure Events, Persisted Evaluation Improvements (#411)
    ### New Features
      - Added Sampling to Gate/Config/Layer non-analytical exposure events
      ### Improvements
      - N / A
      ### Fixes
      - N / A
    
    
    
    
    
    >Included In This Release
    >- a8d349ead176af3bcdff05d41096a295cb73efd7 tore-statsig
    >   - Revert "persisted evaluation with experiment filtering" (#410)
    >- 7a2893aff5067b8315fb0564d88c26ec6162a0b0 kenny-statsig
    >   - persisted evaluation with experiment filtering (#402)
    >- da8a5618892a21591beadf3b90321ea402e42d89 Weihao Ding
    >   - feat: sampling (#406)

--- src/main/kotlin/com/statsig/sdk/StatsigMetadata.kt
+++ src/main/kotlin/com/statsig/sdk/StatsigMetadata.kt
@@ -4,7 +4,7 @@ import com.google.gson.annotations.SerializedName
 import java.util.Properties
 import java.util.UUID
 
-private const val VERSION = "1.33.0"
+private const val VERSION = "1.34.0"
 
 internal data class StatsigMetadata(
     @SerializedName("sdkType") var sdkType: String = "java-server",
