5618b8de1cf72b44e015f39ad701f0a78d709a5e
Weihao Ding <158090588+weihao-statsig@users.noreply.github.com>
2025-04-04 16:53:26 -0700

    [release] 2.0.2 - Sampling Improvements  (#460)

--- src/main/kotlin/com/statsig/sdk/StatsigMetadata.kt
+++ src/main/kotlin/com/statsig/sdk/StatsigMetadata.kt
@@ -4,7 +4,7 @@ import com.google.gson.annotations.SerializedName
 import java.util.Properties
 import java.util.UUID
 
-private const val VERSION = "2.0.1"
+private const val VERSION = "2.0.2"
 
 internal data class StatsigMetadata(
     @SerializedName("sdkType") var sdkType: String = "java-server",
