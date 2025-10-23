49a5cf430fba37a1d84ebf63ba483483e8b53903
Xin Li <137219293+xinlili-statsig@users.noreply.github.com>
2025-10-23 12:44:02 -0700

    1.36.5

--- src/main/kotlin/com/statsig/sdk/StatsigMetadata.kt
+++ src/main/kotlin/com/statsig/sdk/StatsigMetadata.kt
@@ -4,7 +4,7 @@ import com.google.gson.annotations.SerializedName
 import java.util.Properties
 import java.util.UUID
 
-private const val VERSION = "1.36.4"
+private const val VERSION = "1.36.5"
 
 internal data class StatsigMetadata(
     @SerializedName("sdkType") var sdkType: String = "java-server",
