93870cf9de85c1552aa395c7edde08bfe38a650c
Weihao Ding <158090588+weihao-statsig@users.noreply.github.com>
2025-11-21 18:47:38 -0800

    [release] 3.1.2 - Bump GRPC package Version (#503)
    ### Fixes
    - Known vulnerabilities in the io.grpc:grpc-netty-shaded package. Bumped
    to 1.75.0
    
    
    
    
    
    >Included In This Release
    >- 0276a9e0ba1d140b6270a2b45fea50382da29a8b Weihao Ding
    >   - fix: bump grpc version (#502)

--- src/main/kotlin/com/statsig/sdk/StatsigMetadata.kt
+++ src/main/kotlin/com/statsig/sdk/StatsigMetadata.kt
@@ -4,7 +4,7 @@ import com.google.gson.annotations.SerializedName
 import java.util.Properties
 import java.util.UUID
 
-private const val VERSION = "3.1.1"
+private const val VERSION = "3.1.2"
 
 internal data class StatsigMetadata(
     @SerializedName("sdkType") var sdkType: String = "java-server",
